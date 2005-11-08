978b8cec221a547a1c56d5ee79490767f72c9d58
dwc <dwc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-11-08 18:39:42 +0000

    Add time parameter and cleanup
    git-svn-id: https://plugins.svn.wordpress.org/ical-events/trunk@3908 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- test.php
+++ test.php
@@ -3,7 +3,11 @@
 require_once($_ENV['HOME'] . '/public_html/wp-config.php');
 require_once('ical-events.php');
 
-if (count($argv) < 2) die($argv[0] . " URL [NUMBER]\n");
+if (count($argv) < 2) die($argv[0] . " URL [TIME] [NUMBER]\n");
 
-ICalEvents::display_events($argv[1], time(), NULL, (isset($argv[2]) ? $argv[2] : 5));
+$url  = $argv[1];
+$time = (isset($argv[2]) ? $argv[2] : time());
+$num  = (isset($argv[3]) ? $argv[3] : 5);
+
+ICalEvents::display_events($url, $time, NULL, $num);
 ?>
